bd381a64eab83734e14a0cd9eff8f422c9c04818
CamilleGS <camillegon46654@gmail.com>
2025-04-03 22:12:40 -0300

    adding task

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,35 @@
 import React from "react";
 import { Container, ContainerItems, Img, Input, Button, H1, P } from "./style";
 import { useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
 import Check from '../../assets/check.png'
 
 
+//dps de fazer o navigate temos que:
+//pegar os valores dos inputs primeiro, adicionar no array conforme eu coloco
+
 function Home(){
     const navigate = useNavigate()
+    const inputTask = useRef()
+    const [task, setTask] = useState([])
+
+
+        
+    
 
     function goForward(){
-        navigate("/task")
+        
+        if(inputTask.current.value === ""){
+            alert('Escreva uma tarefa')
+        } else{
+            const newTask = inputTask.current.value
+            setTask([...task,newTask])
+            
+            navigate("/task")
+            
+        }
+        
+        
       }
 
 return(
@@ -17,7 +38,7 @@ return(
         <H1>Lista de Tarefas</H1>
         <ContainerItems>
             <P>Tarefa</P>
-            <Input placeholder="Escreva sua tarefa"/>
+            <Input ref={inputTask} placeholder="Escreva sua tarefa"/>
             <Button onClick={goForward}>Adicionar</Button>
         </ContainerItems>
     </Container>
